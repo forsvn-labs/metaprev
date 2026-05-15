f0663617e3fdbb5522b69d625d2f518e960b7fe8
hungv47 <levinhhungg@gmail.com>
2026-05-15 10:38:59 +0700

    chore: bump version from 0.3.2 to 0.3.3

--- bin/metaprev.ts
+++ bin/metaprev.ts
@@ -24,7 +24,7 @@ type Opts = {
   insecure: boolean
 }
 
-const VERSION = '0.3.2'
+const VERSION = '0.3.3'
 const SUBCOMMANDS = new Set<Cmd>(['issues', 'facts'])
 const ISSUE_TAGS: Record<IssueLevel, string> = {
   error: 'ERR',
